65d9d9d9ce442302697b2fac610584350bd0862f
PTSoup <phaimani.souphaphone@gmail.com>
2019-03-13 20:38:12 -0500

    Prep for EBS

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT || 3002;
 
-app.use(morgan('dev'));
 app.use('/:ticker', express.static('public'));
 
 app.listen(port, () => {
